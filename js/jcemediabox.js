1d9e5a8e93aba8edf1c1236abeaa8eeea4be821b
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2013-01-05 16:58:12 +0000

    Encode entities; Use base64 encoded URL for auto popup cookie name

--- js/jcemediabox.js
+++ js/jcemediabox.js
@@ -14,6 +14,154 @@
  */
 (function(window) {
     
+    /**
+    *
+    *  Base64 encode / decode
+    *  http://www.webtoolkit.info/
+    *
+    **/
+    var Base64 = {
+        // private property
+        _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+        // public method for encoding
+        encode : function (input) {
+            var output = "";
+            var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+            var i = 0;
+
+            input = Base64._utf8_encode(input);
+
+            while (i < input.length) {
+
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                Base64._keyStr.charAt(enc1) + Base64._keyStr.charAt(enc2) +
+                Base64._keyStr.charAt(enc3) + Base64._keyStr.charAt(enc4);
+
+            }
+
+            return output;
+        },
+
+        // public method for decoding
+        decode : function (input) {
+            var output = "";
+            var chr1, chr2, chr3;
+            var enc1, enc2, enc3, enc4;
+            var i = 0;
+
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            while (i < input.length) {
+
+                enc1 = Base64._keyStr.indexOf(input.charAt(i++));
+                enc2 = Base64._keyStr.indexOf(input.charAt(i++));
+                enc3 = Base64._keyStr.indexOf(input.charAt(i++));
+                enc4 = Base64._keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+            }
+
+            output = Base64._utf8_decode(output);
+
+            return output;
+
+        },
+
+        // private method for UTF-8 encoding
+        _utf8_encode : function (string) {
+            string = string.replace(/\r\n/g,"\n");
+            var utftext = "";
+
+            for (var n = 0; n < string.length; n++) {
+
+                var c = string.charCodeAt(n);
+
+                if (c < 128) {
+                    utftext += String.fromCharCode(c);
+                }
+                else if((c > 127) && (c < 2048)) {
+                    utftext += String.fromCharCode((c >> 6) | 192);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
+                else {
+                    utftext += String.fromCharCode((c >> 12) | 224);
+                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
+
+            }
+
+            return utftext;
+        },
+
+        // private method for UTF-8 decoding
+        _utf8_decode : function (utftext) {
+            var string = "";
+            var i = 0;
+            var c = 0, c1 = 0, c2 = 0;
+
+            while ( i < utftext.length ) {
+
+                c = utftext.charCodeAt(i);
+
+                if (c < 128) {
+                    string += String.fromCharCode(c);
+                    i++;
+                }
+                else if((c > 191) && (c < 224)) {
+                    c1 = utftext.charCodeAt(i+1);
+                    string += String.fromCharCode(((c & 31) << 6) | (c1 & 63));
+                    i += 2;
+                }
+                else {
+                    c1 = utftext.charCodeAt(i+1);
+                    c2 = utftext.charCodeAt(i+2);
+                    string += String.fromCharCode(((c & 15) << 12) | ((c1 & 63) << 6) | (c2 & 63));
+                    i += 3;
+                }
+
+            }
+            return string;
+        }
+    };
+    
+    // patch in btoa
+    if (!window.btoa) {
+        window.btoa = Base64.encode;
+    }
+    // patch in atob
+    if (!window.atob) {
+        window.atob = Base64.decode;
+    }
+    
     // html5 element support
     var support = {};
     
@@ -43,6 +191,14 @@
     	
         return bool;
     })();
+    
+    var entities = {
+        '\"'    : '&quot;',
+        "'"     : '&#39;',
+        '<'     : '&lt;',
+        '>'     : '&gt;',
+        '&'     : '&amp;'
+    };
     
     /*
      * From Modernizr v2.0.6
@@ -1316,13 +1472,13 @@
             // Set standard GET header
             var contentType = {
                 'Content-type': 'text/html' + encoding
-                };
+            };
 
             // Set URL Encoded / POST header options
             if (this.options.data) {
                 contentType = {
                     'Content-type': 'application/x-www-form-urlencoded' + encoding
-                    };
+                };
             }
             
             extend(this.options.headers, contentType);
@@ -1791,13 +1947,19 @@
                 (new Function("return " + data))();
             }
         },
+        
+        encode : function(s) {
+            return ('' + s).replace(/[<>&\"\']/g, function(c) {
+                return entities[c] || c;
+            });
+        },
 
         /**
          * Get a popup parameter object
          * @param {String} s Parameter string
          */
         params: function(s) {
-            var a = [], x = [];
+            var a = [], x = [], self = this;
 
             if (typeof s == 'string') {
                 // if a JSON string return the object
@@ -1808,7 +1970,7 @@
                 // JCE MediaBox parameter format eg: title[title]
                 if (/\w+\[[^\]]+\]/.test(s)) {                	                	
                     s = s.replace(/([\w]+)\[([^\]]+)\](;)?/g, function(a, b, c, d) {
-                        return '"' + b + '":"' + c + '"' + (d ? ',' : '');
+                        return '"' + b + '":"' + self.encode(c) + '"' + (d ? ',' : '');
                     });
 
                     return this.parseJSON('{' + s + '}');
@@ -1834,7 +1996,7 @@
                             if (!/[^0-9]/.test(d)) {
                                 return '"' + b + '":' + parseInt(d);
                             }
-                            return '"' + b + '":"' + d + '"';
+                            return '"' + b + '":"' + self.encode(d) + '"';
                         }
                         return '';
                     });
@@ -2336,16 +2498,27 @@
          * Process autopopups
          */
         auto: function() {
-            var t = this;
+            var t = this, expires = JCEMediaBox.options.popup.cookie_expiry;
+            // use the current page URL for unique key
+            var url = document.location;
+            // base64 encode key
+            var key = window.btoa(url);            
+            // remove non-word characters
+            key = key.replace(/[^\w]/g, '');
+            // keep it short
+            key = key.substr(0, 24);
+            
             JCEMediaBox.each(this.popups, function(el, i) {
                 if (el.auto) {
                     if (el.auto == 'single') {
-                        var cookie = t.getCookie('jcemediabox_autopopup_' + el.id);
+                        // get cookie
+                        var cookie = t.getCookie('jcemediabox_' + key + '_' + i);
+                        // create cookie with base64 key and expiry
                         if (!cookie) {
-                            var expires = t.options.popup.cookie_expiry;
-                            var dts = new Date(); dts.setHours(expires * 24);
+                            var dts = new Date();
+                            dts.setHours(expires * 24);
                             
-                            t.setCookie('jcemediabox_autopopup_' + el.id, 1, dts);
+                            t.setCookie('jcemediabox_' + key + '_' + i, 1, dts);
                             t.start(el);
                         }
                     } else if (el.auto == 'multiple') {
@@ -2484,11 +2657,11 @@
             }
 
             // check for auto popup
-            if (el.id) {
-                if (/autopopup-(single|multiple)/.test(el.className)) {
-                    auto = /(multiple)/.test(el.className) ? 'multiple' : 'single';
-                }
+            //if (el.id) {
+            if (/autopopup-(single|multiple)/.test(el.className)) {
+                auto = /(multiple)/.test(el.className) ? 'multiple' : 'single';
             }
+            //}
             
             // get group from data object
             group = group || data.group || '';
@@ -2500,7 +2673,7 @@
                 'group'	: DOM.hasClass(el, 'nogroup') ? '' : group,
                 'type'	: data.type || el.type || '',
                 'params': data,
-                'id'	: el.id || '',
+                //'id'	: el.id || '',
                 'auto'	: auto
             });
 

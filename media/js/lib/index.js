5a0eeb835afee056b3360dda36ebfda56522a9d4
Ryan <ryan@widgetfactorylimited.co.uk>
2024-01-31 19:48:38 +0000

    Create index.js

---
+++ media/js/lib/index.js
@@ -0,0 +1,19 @@
+import 'MediaBox.js';
+
+import 'Mime.js';
+import 'Env.js';
+import 'Entities.js';
+import 'Parameter.js';
+import 'Storage.js';
+import 'Convert.js';
+import 'Addons.js';
+import 'Tools.js';
+import 'Svg.js';
+import 'Plugins.js';
+
+// themes
+import '../themes/bootstrap/js/template.js';
+import '../themes/light/js/template.js';
+import '../themes/shadow/js/template.js';
+import '../themes/squeeze/js/template.js';
+import '../themes/standard/js/template.js';
